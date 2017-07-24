4229c8abf5be881fea50639253298b079ab0e6c9
Ken Overholt <kroverholt@gmail.com>
2017-07-24 19:36:11 -0400

    remaining initial updates

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -33,6 +33,16 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  //L5s7
+  VectorXd y = z - H * x;
+  MatrixXd Ht = H.transpose();
+  MatrixXd S = H * P * Ht + R;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P * Ht * Si;
+
+  //new state
+  x = x + (K * y);
+  P = (I - K * H) * P;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -40,4 +50,26 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  //L5s14
+  float x = ekf_.x_(0);
+  float y = ekf_.x_(1);
+  float vx = ekf_.x_(2);
+  float vy = ekf_.x_(3);
+  
+  float rho = sqrt(x*x+y*y);
+  float theta = atan2(y, x);
+  float rho_dot = (x*vx+y*vy)/rho;
+  float z_pred << rho, theta, rho_dot;
+  
+  VectorXd y = z - z_pred;
+  
+  //KRO L5s7
+  MatrixXd Ht = H.transpose();
+  MatrixXd S = H * P * Ht + R;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P * Ht * Si;
+
+  //new state
+  x = x + (K * y);
+  P = (I - K * H) * P;
 }
