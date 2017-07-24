b204c1a618581e2693e8378b2efea6acca30c221
Ken Overholt <kroverholt@gmail.com>
2017-07-24 19:36:25 -0400

    initial updates

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -36,8 +36,17 @@ FusionEKF::FusionEKF() {
     * Finish initializing the FusionEKF.
     * Set the process and measurement noises
   */
-
-
+  Hj = tools.CalculateJacobian(); //KRO set the actual function here
+  H_laser_ << 1, 0, 0, 0,   // L10, S5
+              0, 1, 0, 0;
+  
+  ekf_.F_ = ??;  //KRO 4x4 state transition matrix
+  ekf_.P_ = ??;  //KRO 4x4 matrix
+  
+  // set the acceleration noise components
+  noise_ax = 9; // L5, sect 13, quiz 9
+  noise_ay = 9; // 
+  //KRO finished TODO
 }
 
 /**
@@ -61,19 +70,34 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     // first measurement
     cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
-    ekf_.x_ << 1, 1, 1, 1;
+    ekf_.x_ << 1, 1, 1, 1;  //KRO important for RMSE; first two will be overwritten but should play with the last 2
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       /**
       Convert radar from polar to cartesian coordinates and initialize state.
       */
+      //measurement_pack.raw_measurements_(0) is rho
+      //measurement_pack.raw_measurements_(1) is theta
+      ekf_.x_(0) = measurement_pack.raw_measurements_(0) * cos(measurement_pack.raw_measurements_(1)); //KRO 
+      elf_.x_(1) = measurement_pack.raw_measurements_(0) * sin(measurement_pack.raw_measurements_(1)); //KRO
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
       Initialize state.
       */
+      //measurement_pack.raw_measurements_(0) is x
+      //measurement_pack.raw_measurements_(1) is y
+      ekf_.x_(0) = measurement_pack.raw_measurements_(0); //KRO
+      elf_.x_(1) = measurement_pack.raw_measurements_(1); //KRO
     }
 
+    ekf_.F_ << 1, 0, 0, 0,  //KRO 1 diagonal matrix
+               0, 1, 0, 0,
+               0, 0, 1, 0,
+               0, 0, 0, 1;
+    
+    previous_timestamp = measurement_pack.timestamp_;
+    
     // done initializing, no need to predict or update
     is_initialized_ = true;
     return;
@@ -91,6 +115,24 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
 
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0; //dt expressed in seconds
+  previous_timestamp_ = mesurement_pack.timestamp;
+  
+  float dt_2 = dt * dt;
+  float dt_3 = dt_2 * dt;
+  float dt_4 = dt_3 * dt;
+  
+  //Modify the F matrix so that the time is integrated  L5, S8
+  ekf_.F_(0, 2) = dt;
+  ekf_.F_(1, 3) = dt;
+  
+  // set the process covariance matrix Q as in L5, s9
+  ekf_.Q_ = MatrixXd(4, 4);
+  ekf_.Q_ << noise_ax*(dt_4/4), 0, noise_ax*(dt_3/2), 0,
+	    	  0, noise_ay*(dt_4/4), 0, noise_ay*(dt_3/2),
+		      noise_ax*(dt_3/2), 0, noise_ax*dt_2, 0,
+			  0, noise_ay*(dt_3/2), 0, noise_ay*dt_2;
+  
   ekf_.Predict();
 
   /*****************************************************************************
@@ -105,8 +147,14 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+    ekf_.H_ = Hj; // KRO set to Hj which should be set using the Jacobian funciton in tools.cpp
+    ekf_.R_ = R_radar;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);  //KRO
   } else {
     // Laser updates
+    ekf_.H_ = H_laser;  //KRO (is this good or need to copy the values?)
+    ekf_.R_ = R_laser;  //KRO (is this good or need to copy the values?)
+    ekf_.Update(measurement_pack.raw_measurements_);  //KRO
   }
 
   // print the output
