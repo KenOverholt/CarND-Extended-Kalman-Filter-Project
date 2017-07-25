f775f44c891dd76f01fbb94b132327167a08c3b5
Ken Overholt <kroverholt@gmail.com>
2017-07-24 23:27:20 -0400

    adjust I

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -42,6 +42,8 @@ void KalmanFilter::Update(const VectorXd &z) {
 
   //new state
   x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
   P_ = (I - K * H_) * P_;
 }
 
@@ -71,5 +73,7 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
 
   //new state
   x = x + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
   P_ = (I - K * H_) * P_;
 }
